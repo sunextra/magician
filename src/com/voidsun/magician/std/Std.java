6de7083db76714255381ed008e0dfd5dc0da4011
sunzhongjian <sunzhongjian@lianjia.com>
2015-08-13 18:30:12 +0800

    first commit

---
+++ src/com/voidsun/magician/std/Std.java
@@ -0,0 +1,14 @@
+package com.voidsun.magician.std;
+
+/**
+ * @Description
+ * @Author voidsun
+ * @Date 2015/8/12
+ * @Email voidsun@126.com
+ */
+public class Std {
+
+    public static int max(int a, int b){
+        return a > b ? a : b;
+    }
+}
