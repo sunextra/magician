49bb14ed9d50bab2a5950090333cbc390bac3f97
voidsun <voidsun@126.com>
2015-11-10 09:28:24 +0800

    optmize

--- src/main/java/com/voidsun/magician/leetcode/ExcelSheetColumnNumber.java
+++ src/main/java/com/voidsun/magician/leetcode/ExcelSheetColumnNumber.java
@@ -21,8 +21,8 @@ package com.voidsun.magician.leetcode;
 public class ExcelSheetColumnNumber {
     public int titleToNumber(String s) {
         int result = 0;
-        for(char c : s.toCharArray()){
-            result = result * 24 + c -64;
+        for(int i=0; i<s.length(); i++){
+            result = result * 26 + s.charAt(i) -64;
         }
         return result;
     }
